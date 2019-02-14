4e9ff5cf44475f63d5c140e8e2cbc77f52c32e3a
Faisal Sajjad <illusionfs@gmail.com>
2019-02-13 19:13:25 -0800

    second commit

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -120,21 +120,9 @@ export const pageQuery = graphql`
               }
             }
           }
-          ... on PrismicPostBodyImage {
-            slice_type
-            id
-            primary {
-              image {
-                localFile {
-                  childImageSharp {
-                    fluid(maxWidth: 1200, quality: 90) {
-                      ...GatsbyImageSharpFluid_withWebp
-                    }
-                  }
-                }
-              }
-            }
-          }
+
+
+
         }
       }
     }
---
+++ src/templates/postx.jsx
@@ -0,0 +1,15 @@
+          ... on PrismicPostBodyImage {
+            slice_type
+            id
+            primary {
+              image {
+                localFile {
+                  childImageSharp {
+                    fluid(maxWidth: 1200, quality: 90) {
+                      ...GatsbyImageSharpFluid_withWebp
+                    }
+                  }
+                }
+              }
+            }
+          }
